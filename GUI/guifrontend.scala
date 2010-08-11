47b325c195de30a70a74d9ce13d5e7ad2a5e7832
David Renshaw <renshaw@cmu.edu>
2010-08-10 22:59:59 -0400

    playing with swing

--- GUI/guifrontend.scala
+++ GUI/guifrontend.scala
@@ -3,29 +3,34 @@ package DLBanyan.GUI
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 
-import scala.swing.Frame
+import scala.swing._
+import javax.swing._
 
-
-class FrontEnd extends Frame {
+class FrontEnd extends JFrame("SWEET")  {
   
   def test : Unit = {
     println("hello world")
   }
 
+/*
   override def closeOperation() : Unit = {
     System.exit(0)
   }
-
+*/
 
 }
 
 object FE {
 
   def main : Unit = {
-    val w = new FrontEnd();
-    w.size = new java.awt.Dimension(400, 320)
-    w.centerOnScreen
-    w.visible = true
+    val w = new FrontEnd 
+           
+    w.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    w.setSize(420, 300)
+//    w.centerOnScreen
+    w.setVisible(true)
+    println(w)
+
   }
 
 }
