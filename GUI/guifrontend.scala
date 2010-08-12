92147d7c6d8e4b4c004132b607de940f74231eaa
David Renshaw <renshaw@cmu.edu>
2010-08-12 11:01:00 -0400

    starting node output in hierarchical layout

--- GUI/guifrontend.scala
+++ GUI/guifrontend.scala
@@ -13,6 +13,8 @@ import com.mxgraph.layout._
 import scala.actors.Actor
 import scala.actors.Actor._
 
+import DLBanyan.Nodes._
+
 class FrontEnd(fa: Actor) extends JFrame("PROVER")  {
   
   val frontactor = fa
@@ -33,11 +35,17 @@ class FrontEnd(fa: Actor) extends JFrame("PROVER")  {
     val v2 = graph.insertVertex(gparent, null, "World!", 0, 0, 80, 30)
     val v3 = graph.insertVertex(gparent, null, "kthxbye", 0, 0, 70, 20)
     val v4 = graph.insertVertex(gparent, null, "?", 0, 0, 60, 20)
+    val v5 = graph.insertVertex(gparent, null, "5", 0, 0, 50, 20)
+    val v6 = graph.insertVertex(gparent, null, "6", 0, 0, 50, 20)
+    val v7 = graph.insertVertex(gparent, null, "7", 0, 0, 50, 20)
     graph.insertEdge(gparent, null, "Edge", v1, v2)
     graph.insertEdge(gparent, null, "Edge2", v1, v3)
     graph.insertEdge(gparent, null, "Edge3", v1, v4)
+    graph.insertEdge(gparent, null, "Edge4", v2, v5)
+    graph.insertEdge(gparent, null, null, v5, v6)
+    graph.insertEdge(gparent, null, null, v6, v7)
 //val glayout = new com.mxgraph.layout.hierarchical.mxHierarchicalLayout(graph)
-    val glayout = new orthogonal.mxOrthogonalLayout(graph)
+    val glayout = new hierarchical.mxHierarchicalLayout(graph)
     glayout.execute(gparent)
 //    val lm = new mxLayoutManager(graph) { 
 //      val layout = new mxCompactTreeLayout(graph)
@@ -56,12 +64,33 @@ class FrontEnd(fa: Actor) extends JFrame("PROVER")  {
   }
 		
 
-
-
   final val  graphComponent : mxGraphComponent = new mxGraphComponent(graph)
   getContentPane().add(graphComponent)
 
 
+  val nodeMap = new scala.collection.mutable.HashMap[NodeID,Object]()
+
+  def addProofNode(nd: ProofNode): Unit = {
+    val v = graph.insertVertex(gparent, null, nd.nodeID.toString, 0,0,50,20)
+    nodeMap.put(nd.nodeID, v)
+//    for(c <- nd.children) {
+//      graph.insertEdge(gparent, null, null,)
+//    }
+  }
+
+  def addEdges(nd: ProofNode): Unit = {
+    nodeMap.get(nd.nodeID) match {
+      case Some(v) => 
+        for(c <- nd.children) {
+          nodeMap.get(c) match {
+            case Some(v1) => 
+              graph.insertEdge(gparent, null, "", v, v1)
+            case None => 
+          }
+        }
+      case None => 
+    }
+  }
 
 
   def test : Unit = {
@@ -85,7 +114,7 @@ object FE {
     val w = new FrontEnd(fa)
            
     w.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    w.setSize(840, 600)
+    w.setSize(100, 100)
 //    w.centerOnScreen
     w.setVisible(true)
     println(w)
