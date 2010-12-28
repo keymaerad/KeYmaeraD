257fbc0216d5997c0eb12621cfa943a30c794822
David Renshaw <dwrenshaw@gmail.com>
2010-12-28 11:06:23 -0500

    types for qhp

--- types.scala
+++ types.scala
@@ -9,6 +9,9 @@ abstract class Pred
 case class R(r: String, ps: List[Term]) extends Pred
 
 
+sealed abstract class Type
+case class typ(nm: String) extends Type
+
 // first order formula
 sealed abstract class Formula
 case object True extends Formula
@@ -29,6 +32,7 @@ case class SchemaVar(v: String) extends Formula
 sealed abstract class HP
 case class Assign(s: String, v: Term) extends HP
 case class AssignAny(s: String) extends HP
+case class AssignQuantified(i : String, c: Type, f : Fn, v: Term)
 case class Check(h: Formula) extends HP
 case class Seq(p1: HP, p2: HP) extends HP
 case class Choose(p1: HP, p2: HP) extends HP
@@ -39,7 +43,12 @@ case class Evolve(derivs: List[(String,Term)],
                   h: Formula,
                   inv_hints: List[Formula],
                   sols: List[Formula]) extends HP
-
+case class EvolveQuantified(i:String, 
+                            c: Type,
+                            f : Fn, 
+                            v : Term,
+                            h : Formula
+                            ) extends HP
 
 
 
