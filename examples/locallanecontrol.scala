ffca18e932d129939c3f5fea47a9838261a46518
David Renshaw <renshaw@cmu.edu>
2011-01-19 13:33:29 -0500

    getting there...

--- examples/locallanecontrol.scala
+++ examples/locallanecontrol.scala
@@ -2,11 +2,15 @@ dl('load, "examples/locallanecontrol.dl")
 val rl = loopInduction(
   parseFormula(
     "(~(f() = l()) & eps() >= 0) &" + 
-    "((b()*B()*x(l()) > b()*B()*x(f()) + " + 
+    "(((b()*B()*x(l()) > b()*B()*x(f()) + " + 
     "(1/2) * (B()*v(f())^2 -  b()*v(l())^2) & " +
     "v(l()) > x(f()) &" +
     "v(f()) >= 0 &" +
-    "v(l()) >= 0 ) | v(l()) < x(f()) | l() = f())"))
+    "v(l()) >= 0 ) | v(l()) < x(f()) | l() = f()))"))
 dl('gotoroot)
 dl('tactic, trylistofrulesT(List(rl)))
 dl('tactic, applyToLeavesT(tryruleatT(close) (RightP(0))))
+dl('tactic, applyToLeavesT(repeatT(alphaT)))
+dl('tactic, applyToLeavesT(tryruleatT(close) (RightP(0))))
+dl('tactic, applyToLeavesT(repeatT(eitherT(hpeasyT,alphaT))))
+
