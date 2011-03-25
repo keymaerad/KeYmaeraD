66a7d0e05fe27619fc1f4b8a87d1b9177cec9abf
David Renshaw <dwrenshaw@gmail.com>
2011-03-25 10:52:02 -0400

    closed the hard (i think) branch

--- examples/headache.scala
+++ examples/headache.scala
@@ -1,17 +1,31 @@
 dl('load, "examples/headache.dl")
 
 
+val cuttct0 = cutT(
+  StandardCut,
+  parseFormula(
+    "2*B()*X1>2*B()*X2+ V2^2 - V1^2 +" + 
+     "(A()+B())*(A()*eps()^2+2 * eps()*V2)"
+  ),
+  parseFormula(
+     "(A()+B())*(A()*eps()^2+2 * eps()*V2) >= "+
+     "(A()+B())*(A()*s()^2+2 * s()*V2) "
+  )
+)
+
+
 val cuttct = cutT(
   DirectedCut,
   parseFormula(
-    "b()*B()*X1>b()*B()*X2+1/2*(B()*V1^2-b()*V2^2)+" + 
-     "B()*(A()+b())*(1/2*A()*eps()^2+eps()*V1)"
+    "2*B()*X1>2*B()*X2+ V2^2 - V1^2 +" + 
+     "(A()+B())*(A()*eps()^2+2 * eps()*V2)"
   ),
   parseFormula(
-    "b()*B()*X1>b()*B()*X2+1/2*(B()*V1^2-b()*V2^2)+" + 
-     "B()*(A()+b())*(1/2*A()*s()^2+s()*V1)"
+    "2*B()*X1>2*B()*X2+ V2^2 - V1^2 +" + 
+     "(A()+B())*(A()*s()^2+2 * s()*V2)"
   )
 )
+
 
 val okcuttct = cutT(
   StandardCut,
@@ -24,6 +38,12 @@ val okcuttct = cutT(
   )
 )
 
+
+val safetyfm=
+  parseFormula(
+    "(forall l:C . x(F)<x(l)&~F=l&(forall i:C.~i=F&~i=l==>x(i)<x(F)"+
+    "|x(l)<x(i))==>2*B()*x(l)>2*B()*x(F)+v(F)^2-v(l)^2+(A()+B())*(A()*eps()^2+2*eps()*v(F)))"
+  )
 
   
 val mostthingsT = 
@@ -47,27 +67,68 @@ val everythingT: Tactic =
 
 
 
+val provecut =
+ composelistT(
+   tryrulepredT(hide) (fm => fm match {case Binop(Or,_,_) => true case _ => false}),
+   tryruleT(not),
+   nullarizeT*,
+   tryruleunifyT(hide) (
+     parseFormula( "X1=1/2*A*s()^2+V*s()+X2")
+   )*,
+   tryruleunifyT(hide) (
+     parseFormula( "V1=A*s()+V")
+   )*,
+   tryruleunifyT(hide) (
+     parseFormula( "2*B()*X2>2*B()*X1 + V1^2-V2^2")
+   )*,
+   substT*,
+   hidethencloseT
+ )
+
+val hardcase = 
+  composelistT(
+    nullarizeT*,
+    substT*,
+    hidethencloseT
+  )
+
+
 val impsg1 = 
   composelistT(
-    tryruleT(orLeft)*,
-    tryruleT(andLeft)*
+    tryruleT(andLeft)*,
+    hideunivsT(St("C")),
+    branchT(cuttct,
+            List(provecut, 
+                 branchT(tryruleT(orLeft),
+                         List(branchT(tryruleT(orLeft),
+                                      List(hardcase, 
+                                           composelistT(nullarizeT*,hidethencloseT))),
+                              composelistT(
+                                tryruleT(andLeft)*,
+                                hideunivsT(St("C")),
+                                nullarizeT*,
+                                substT*,
+                                hidethencloseT
+                              )
+                            )
+                       )
+               )
+          )
   )
 
-val origok = 
-  composelistT(
-    tryruleT(andRight)*
-  )
 
+val orsgtct0 = unitT
 
-val orsg1tct = unitT 
+val orsg1tct = 
+  branchT(tryruleT(orLeft), List(orsgtct0))
 
 val orsg2tct = 
  composelistT(
-   tryruleT(andLeft)*,
+   tryruleunifyT(andLeft)(Binop(And, safetyfm, Atom(R("=",List(Var("YY"),Var("ZZ")))))),
    instantiate1T(St("C")),
    vacuousT*,
    branchT(tryruleT(impLeft),
-           List(impsg1,origok))
+           List(impsg1,tryruleT(close)))
  )
 
 
