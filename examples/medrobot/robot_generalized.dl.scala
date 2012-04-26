4ab430d62da381265df86c82da6a507c90421449
Kouskoulas <kouskya1@cmu-810419.wv.cc.cmu.edu>
2012-04-26 14:59:46 -0400

    improved branch 5

--- examples/medrobot/robot_generalized.dl.scala
+++ examples/medrobot/robot_generalized.dl.scala
@@ -6,9 +6,9 @@ val loopInv =
 
 val easybranchT = 
   composelistT(
-    hpalphaT*,
+    hpalpha1T*,
     diffsolveT(RightP(0), Endpoint),
-    hpalphaT*,
+    hpalpha1T*,
       tryruleT(andRight)<(
       easiestT,
       alleasyT
@@ -40,16 +40,21 @@ val cutb2 =
 
 
 
-val cutb5 = 
-  cutT(
+val cutb5a = 
+  cutmT(
     StandardCut,
-    parseFormula("TMP * TMP * K() = 2 * ((qx() - px()) * nx() + (qy() - py()) * ny()) * (FXP * nx() + FYP * ny())"),
-    parseFormula("((qx() - px()) + K() * (fx() - (fx() * nx() + fy() * ny() - TMP) * nx()) * e() + " +
-                 "1 / 2 * K() * FXP * e()^2) * nx() + " +
-                 "((qy() - py()) + K() * (fy() - (fx() * nx() + fy() * ny() - TMP) * ny()) * e() + " + 
-                 "1 / 2 * K() * FYP * e()^2) * ny() >= 0"
-               )
-  )
+    List(parseFormula("TMP * K() * e() = 1"),
+         parseFormula("FXP * nx() + FYP * ny() > 0")),
+    parseFormula("((qx() - px()) * nx() + (qy() - py()) * ny()) + " +
+                 "(K() * ((fx() * nx() + fy() * ny()) - " +
+		 "((fx() * nx() + fy() * ny()) + ((qx() - px()) * nx() + " +
+		 "(qy() - py()) * ny() + 1 / 2 * K() * (FXP * nx() + FYP * " +
+		 "ny()) * e()^2) * TMP)) * s()) " +
+		 "+ 1 / 2 * K() * (FXP * nx() + FYP * ny())* s()^2 >= 0"))
+
+
+
+
 
 val cutb6 = 
  cutT(
@@ -68,30 +73,31 @@ val main =
    tryruleT(loopInduction(loopInv))<(
      easiestT,
      composelistT(
-       hpalphaT*,
+       hpalpha1T*,
        tryruleT(andRight)<(
          composelistT(
-           hpalphaT*,
+           hpalpha1T*,
            tryruleT(andRight)<(
              composelistT(
-               hpalphaT*,
+               hpalpha1T*,
                tryruleT(andRight)<(
                  composelistT(
-                   hpalphaT*,
+                   hpalpha1T*,
                    tryruleT(andRight)<(
                      composelistT(
-                       hpalphaT*,
+                       hpalpha1T*,
                        tryruleT(andRight)<(
                          composelistT(
-                           hpalphaT*,
+                           hpalpha1T*,
                            tryruleT(andRight)<(
                              // branch 1
                              easybranchT,
+
                              // branch 2
                              composelistT(
-                               hpalphaT*,
+                               hpalpha1T*,
                                diffsolveT(RightP(0), Endpoint),
-                               hpalphaT*,
+                               hpalpha1T*,
                                tryruleT(andRight)<(
                                  easiestT,
                                  composelistT(
@@ -111,9 +117,9 @@ val main =
                          ),
                          //branch 3
                          composelistT(
-                           hpalphaT*,
+                           hpalpha1T*,
                            diffsolveT(RightP(0), Endpoint),
-                           hpalphaT*,
+                           hpalpha1T*,
                            tryruleT(andRight)<(
                              easiestT,
                              hidehasfnT("e")& alleasyT
@@ -125,46 +131,80 @@ val main =
                      easybranchT
                    )
                  ),
-                 // branch 5
-                 composelistT(
-                   hpalphaT*,
-                   diffsolveT(RightP(0), Endpoint),
-                   hpalphaT*,
-                   tryruleT(andRight)<(
-                     easiestT,
-                     composelistT(
-                       substT*,
-                       cutb5<(
-                         composelistT(
-                           hidehasfnT("s")*,
-                           unsubT(
-                             parseFormula(
-                               "TMPK = 2 * (X * nx() + Y * ny()) * (FXP * nx() + FYP * ny())"
-                             ),
-                             List(Var("X"), Var("Y"))
-                           ),
-                           tryruleatT(hide)(LeftP(6)),
-                           arithT
 
-                         ),
-                         composelistT(
-                           tryruleatT(hide)(LeftP(7)),
-                           tryruleatT(hide)(LeftP(8)),
-                           tryruleatT(hide)(LeftP(3)),
-                           arithT
-                         )
-                       )
-                     )
-                   )
-                 )
+		 // branch 5
+                  composelistT(
+	        	   hpalpha1T*,
+	        	   tryruleT(andRight)<(
+	        	    composelistT(hpalpha1T*,
+	        	                 tryruleT(andRight)<(
+	        			 composelistT(hpalpha1T*,
+                                              diffsolveT(RightP(0), Endpoint),
+	        			      hpalpha1T*,
+	        			      tryruleT(andRight)<(
+                                                  easiestT,
+	        			          composelistT(
+						             substT*, 
+	        			                     tryruleatT(hide)(LeftP(14)),
+	        			                     tryruleatT(hide)(LeftP(12)),
+	        			                     tryruleatT(hide)(LeftP(10)),
+	        			                     tryruleatT(hide)(LeftP(6)),
+	        			                     tryruleatT(hide)(LeftP(2)),
+	        			                     tryruleatT(hide)(LeftP(0)),
+	        			                     arithT
+	        			          )
+                                             )
+                                         ),
+	        			 composelistT(hpalpha1T*,
+                                              diffsolveT(RightP(0), Endpoint),
+				              hpalpha1T*,
+					      tryruleT(andRight)<(
+                                                  easiestT,
+						  composelistT(
+						             substT*,
+	        			                     tryruleatT(hide)(LeftP(9)),
+	        			                     tryruleatT(hide)(LeftP(8)),
+							     cutb5a<(composelistT(
+     								     tryruleatT(hide)(LeftP(6)),
+								     unsubT(parseFormula(
+								     "FN <= 0"),
+							              List(Var("FN"))),
+								      unsubT(parseFormula(
+								      "Q + K() * (A * e() + FNP * e()^2 * (1 / 2)) <= 0"),
+								      List(Var("Q"),Var("FNP"))),
+								      arithT),
+	        			                             composelistT(
+								     tryruleatT(hide)(LeftP(11)),
+								     tryruleatT(hide)(LeftP(10)),
+								     tryruleatT(hide)(LeftP(9)),
+	        			                             tryruleatT(hide)(LeftP(8)),
+	        			                             tryruleatT(hide)(LeftP(7)),
+	        			                             tryruleatT(hide)(LeftP(6)),
+	        			                             tryruleatT(hide)(LeftP(5)),
+	        			                             tryruleatT(hide)(LeftP(4)),
+	        			                             tryruleatT(hide)(LeftP(3)),
+	        			                             tryruleatT(hide)(LeftP(2)),
+	        			                             tryruleatT(hide)(LeftP(1)),
+							             arithT))
 
-               )
+                                                  )
+                                              )
+                                         )
+                                       ), nilT),
+                      composelistT(hpalpha1T*,
+		                diffsolveT(RightP(0), Endpoint), 
+                                hpalpha1T*,
+                                tryruleT(andRight)<(
+                                  easiestT,
+                                  composelistT(substT*,arithT))
+                               )))
+              )
              ),
              // branch 6
              composelistT(
-               hpalphaT*,
+               hpalpha1T*, 
                diffsolveT(RightP(0), Endpoint),
-               hpalphaT*,
+               hpalpha1T*,
                tryruleT(andRight)<(
                  easiestT,
                  composelistT(
@@ -182,10 +222,10 @@ val main =
              )
            )
          ),
+	 // branch 7
          easybranchT
        )
      ),
      easiestT
    )
-
 }
